e5eaa35d62f4e7d8cea745a928079fe7814b5b2e
article2016 <409791659@qq.com>
2017-06-07 10:16:35 +0800

    mysql

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,4 +1,14 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	// 添加数据库配置信息
+	'DB_TYPE'=>'mysql',// 数据库类型
+	'DB_HOST'=>'mysql://mysql:3306/',// 服务器地址
+	'DB_NAME'=>'wordpressdb',// 数据库名
+	'DB_USER'=>'wordpress',// 用户名
+	'DB_PWD'=>'jason2016',// 密码
+	'DB_PORT'=>3306,// 端口
+	'DB_PREFIX'=>'tbl_',// 数据库表前缀
+	'DB_CHARSET'=>'utf8',// 数据库字符集
+	'__PUBLIC__'=>_ROOT_.'/PUBLIC',
 );
